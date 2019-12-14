04d02f11c57d96e9ce8117955e26da3239426644
NAPOLEON039 <nihar.raote@gmail.com>
2019-12-14 18:50:19 +0530

    Specified package name for importing firebase and notes are now added to the database

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,13 @@
     <textarea v-model="content" name="content" id="note-content" cols="40" rows="8" placeholder="Type note here"></textarea> <button @click="submitNote" id="submit-note">Add note</button>
     <p>Your notes:</p>
     <ul id="notes">
-      <li :key="index" v-for="(note, index) in notes"> {{note.title}} - {{note.body}} </li>
+      <li :key="index" v-for="(note, index) in notes"> {{note.title}} - {{note.content}} </li>
     </ul>
   </div>
 </template>
 
 <script>
+import db from './firebaseInit';
 
 export default {
   name: 'app',
@@ -23,11 +24,22 @@ export default {
   },
   methods: {
     submitNote() {
+      db.collection('notes').add({
+        title: this.title,
+        content: this.content
+      })
+      // .then(docRef => {
+      //   this.notes.push({
+      //     title: this.title,
+      //     content: this.content
+      //   })
+      // })
+      .catch(error => console.log(error))
       this.notes.push({
         title: this.title,
-        body: this.content
-      });
-      this.title = '',
+        content: this.content
+      })
+      this.title = ''
       this.content = ''
     }
    }
