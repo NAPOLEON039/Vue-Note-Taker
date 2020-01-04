4db21464ffb400a5b6f87c50415f7b152d8f257f
NAPOLEON039 <nihar.raote@gmail.com>
2020-01-04 11:40:42 +0530

    Notes are fetched from database
    New notes could be added to database but weren't being fetched from database. Now when component is mounted, previously created notes are fetched from database

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,10 @@
     <textarea v-model="content" name="content" id="note-content" cols="40" rows="8" placeholder="Type note here"></textarea> <button @click="submitNote" id="submit-note">Add note</button>
     <p>Your notes:</p>
     <ul id="notes">
-      <li :key="index" v-for="(note, index) in notes"> {{note.title}} - {{note.content}} </li>
+      <li :key="index" v-for="(note, index) in notes"> 
+        <h3>{{note.title}}</h3>
+        <p>{{note.content}}</p>
+      </li>
     </ul>
   </div>
 </template>
@@ -19,28 +22,43 @@ export default {
     return {
       title: '',
       content: '',
+      date: '',
       notes: []
     }
   },
+  mounted() {
+    db.collection('notes').get().then(query => {
+      query.forEach(doc => {
+        const dbNote = {
+          title: doc.data().title,
+          content: doc.data().content
+        }
+        this.notes.push(dbNote)
+      })
+    })
+    this.sortNotes();
+    console.log(this.notes);
+    // this.notes.sort((a, b) => a.date.getTime() - b.date.getTime());
+  },
   methods: {
     submitNote() {
-      db.collection('notes').add({
-        title: this.title,
-        content: this.content
-      })
-      // .then(docRef => {
-      //   this.notes.push({
-      //     title: this.title,
-      //     content: this.content
-      //   })
-      // })
-      .catch(error => console.log(error))
-      this.notes.push({
-        title: this.title,
-        content: this.content
-      })
-      this.title = ''
-      this.content = ''
+      if(this.title.length > 0 && this.content.length > 0) {
+        db.collection('notes').add({
+          title: this.title,
+          content: this.content
+        })
+        .catch(error => console.log(error))
+        this.notes.push({
+          title: this.title,
+          content: this.content
+        })
+        this.title = ''
+        this.content = ''
+      }
+    },
+    sortNotes() {
+      this.notes = this.notes.sort((a, b) => (a.title > b.title) ? 1 : -1);
+      console.log('Sorted')
     }
    }
 }
@@ -48,6 +66,6 @@ export default {
 
 <style>
 #notes li{
-  white-space: pre;
+  white-space: pre-wrap;
 }
 </style>
